69a77b3a494174bd58fc4e6d2440041fffe4d457
Dmitry Lakhov <bobrokerson@gmail.com>
2024-05-30 16:40:17 +0200

    Update silution

--- Module_1/m1_hello_world.py
+++ Module_1/m1_hello_world.py
@@ -2,7 +2,8 @@
 
 print("Hello World!")
 print('We can make the world a little bit better')
-
+out = 'Person'
+print(f"I'm {out}")
 """
 To run this script you need to add execute bit to a file (chmod +x)
 You can run it with  python3 m1_hello_world.py or with ./m1_hello_world.py
